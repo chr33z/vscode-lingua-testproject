6f9d83791cfc8e9212f887aff1368003ffd6c3d7
Christopher Gebhardt <c.gebhardt@it-labs.de>
2020-05-26 18:22:17 +0200

    Added more test files

--- test-project/src/app/app.component.ts
+++ test-project/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test-project';
+    public title = 'test-project';
+    public combinedTranslation = '';
+
+    constructor(private translate: TranslateService) {
+        this.combinedTranslation = translate.instant('combine.' + '0');
+    }
 }
